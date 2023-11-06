5145795af78da8a0cfaabc862845fa1e3ecd5f09
yournewgoodname <yournew@email.com>
2023-11-05 16:02:26 -0800

    ship position

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ let boardWidth = tileSize * columns; // 32 * 16
 let boardHeight = tileSize * rows; // 32 * 16
 let context;
 
+// ship position
+let shipWidth = tileSize*2;
+let shipHeight = tileSize;
+let shipX = tileSize * columns/2 - tileSize;
+let shipY = tileSize * rows - tileSize*2;
+
+let ship = {
+    x : shipX,
+    y : shipY,
+    width : shipWidth,
+    height : shipHeight
+}
