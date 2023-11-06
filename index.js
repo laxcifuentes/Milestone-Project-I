0af8f06469f1867c73b343b3120576d4654f2e27
yournewgoodname <yournew@email.com>
2023-11-05 16:00:37 -0800

    board setup

---
+++ index.js
@@ -0,0 +1,10 @@
+// inital board setup
+let tileSize = 32;
+let rows = 16;
+let columns = 16;
+
+let board;
+let boardWidth = tileSize * columns; // 32 * 16
+let boardHeight = tileSize * rows; // 32 * 16
+let context;
+
